f37da28ace4af609d518ace578670b4c32e6438d
mAks-1 <chesherow22d@gmail.com>
2021-11-01 13:56:15 +0300

    calculator

---
+++ calculator/js/script.js
@@ -0,0 +1,109 @@
+// document.getElementById("text").textContent = "bye"
+
+let firstValue, operate;
+let qwe = [1,2,3,4]
+
+document.getElementById("one").addEventListener("click", oneFunction);
+function oneFunction() {
+    document.getElementById("screen").textContent += "1"
+}
+
+document.getElementById("two").addEventListener("click", twoFunction)
+function twoFunction() {
+    document.getElementById("screen").textContent += "2"
+}
+
+document.getElementById("three").addEventListener("click", threeFunction)
+function threeFunction() {
+    document.getElementById("screen").textContent += "3"
+}
+
+document.getElementById("four").addEventListener("click", fourFunction)
+function fourFunction() {
+    document.getElementById("screen").textContent += "4"
+}
+
+document.getElementById("five").addEventListener("click", fiveFunction)
+function fiveFunction() {
+    document.getElementById("screen").textContent += "5"
+}
+
+document.getElementById("six").addEventListener("click", sixFunction)
+function sixFunction() {
+    document.getElementById("screen").textContent += "6"
+}
+
+document.getElementById("seven").addEventListener("click", sevenFunction)
+function sevenFunction() {
+    document.getElementById("screen").textContent += "7"
+}
+
+document.getElementById("eight").addEventListener("click", eightFunction)
+function eightFunction() {
+    document.getElementById("screen").textContent += "8"
+}
+
+document.getElementById("nine").addEventListener("click", nineFunction)
+function nineFunction() {
+    document.getElementById("screen").textContent += "9"
+}
+
+document.getElementById("plus").addEventListener("click", plusFunction);
+
+
+function plusFunction() {
+    // operate = 1;
+    operate = qwe[0]
+    firstValue = document.getElementById("screen").textContent;
+    document.getElementById("screen").textContent = ""
+}
+
+
+document.getElementById("minus").addEventListener("click", minusFunction);
+
+function minusFunction() {
+    operate = qwe[1]
+    firstValue = document.getElementById("screen").textContent;
+    document.getElementById("screen").textContent = ""
+}
+
+document.getElementById("multiplication").addEventListener("click", multiplicationFunction);
+
+function multiplicationFunction() {
+    operate = qwe[2]
+    firstValue = document.getElementById("screen").textContent;
+    document.getElementById("screen").textContent = ""
+}
+
+document.getElementById("division").addEventListener("click", divisionFunction);
+
+function divisionFunction() {
+    operate = qwe[3]
+    firstValue = document.getElementById("screen").textContent;
+    document.getElementById("screen").textContent = ""
+}
+
+
+// document.getElementById("equals").addEventListener("click", equalsFunction)
+// function equalsFunction() {
+//     if(operate == 1) {
+//     document.getElementById("screen").textContent = parseInt(firstValue) + parseInt(document.getElementById("screen").textContent)
+//     } else {
+//         document.getElementById("screen").textContent = parseInt(firstValue) - parseInt(document.getElementById("screen").textContent)
+//     }
+// }
+
+
+document.getElementById("equals").addEventListener("click", equalsFunction)
+function equalsFunction() {
+    if(operate == 1) {
+    document.getElementById("screen").textContent = parseInt(firstValue) + parseInt(document.getElementById("screen").textContent)
+    } else if(operate == 2) {
+    document.getElementById("screen").textContent = parseInt(firstValue) - parseInt(document.getElementById("screen").textContent)
+    } else if(operate == 3) {
+    document.getElementById("screen").textContent = parseInt(firstValue) * parseInt(document.getElementById("screen").textContent)
+    } else {
+    document.getElementById("screen").textContent = parseInt(firstValue) / parseInt(document.getElementById("screen").textContent)
+    }
+}
+
--- mmm/js/script.js
+++ mmm/js/script.js
@@ -1,2 +1,14 @@
-// document.getElementById(“”my_button).addEventListener("click", onAllClick);
-// document.getElementById(“my_button").addEventListener("click", onAllClick);
+let r = Math.floor(Math.random() * (256));
+let g = Math.floor(Math.random() * (256));
+let b = Math.floor(Math.random() * (256));
+let colorq = '#' + r.toString() + g.toString() + b.toString();
+
+
+
+
+function onClick() {
+document.getElementById("text").style.color = colorq
+//     document.getElementById("text").style.color = 'rgb('+ 
+//     Math.floor(Math.random() * 255) + 1 + ',' + Math.floor(Math.random() * 255) + 1 
+//     +',' + Math.floor(Math.random() * 255) + 1 +')';
+}
